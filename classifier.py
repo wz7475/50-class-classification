7deb891ab649967174d04510b7f8182259d7a803
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2023-06-11 16:29:19 +0200

    add readme and clean up

---
+++ classifier.py
@@ -0,0 +1,44 @@
+from torch import nn
+
+
+class MyClassifier(nn.Module):
+    def __init__(self, num_classes=50):
+        super().__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+
+        )
+
+        self.fc_layers = nn.Sequential(
+            nn.Linear(256 * 8 * 8, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = torch.flatten(x, 1)
+        x = self.fc_layers(x)
+        return x
---
+++ readme.md
@@ -0,0 +1,9 @@
+# 50-class img classification
+## Taks
+Implement classifier for images, there 50 classes (items, cars, animals etc.)
+![Alt text](imgs.png)
+
+## Scores
+implementation of model `classifier.py`
+
+As feedback - accuracy on test data: `59%` (on 50 unbalanced classed)
